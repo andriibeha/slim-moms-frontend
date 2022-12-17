f41ef01ec3d61a02f951ae53ab0f9f20b65d11f1
andriibeha <beha.andrii@gmail.com>
2022-12-17 18:57:28 +0200

    add theme.jsx add colors variable

---
+++ src/components/Theme.jsx
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    grey: '#E0E0E0',
+    blue: '#264061',
+
+    textFirst: '#9B9FAA',
+    textSecond: '#212121',
+    accent: '#FC842D',
+
+    backdrop: 'rgba(33, 33, 33, 0.12);',
+  },
+};
