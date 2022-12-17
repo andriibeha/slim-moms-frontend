f430d503f949438f1f100bd94cdc97b2a7db68e6
andriibeha <beha.andrii@gmail.com>
2022-12-17 16:14:44 +0200

    fixed/button way

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 // import Loader from 'components/Loader/Loader';
 import { Logo } from 'components/Logo';
-import { Button } from 'components/Button/Button';
+import { Button } from 'components/Button';
 
 export const Home = () => {
   return (
