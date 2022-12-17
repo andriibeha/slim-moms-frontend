2addd073b85f788d6eb6c9eec1efd380967b0365
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-12-17 15:58:15 +0200

    Button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
-import Loader from 'components/Loader/Loader';
+// import Loader from 'components/Loader/Loader';
 import { Logo } from 'components/Logo';
+import { Button } from 'components/Button/Button';
 
 export const Home = () => {
   return (
     <>
       <Logo />
-      <Loader />
+      {/* <Loader /> */}
+      <Button text="Hello" w="182px" h="44px" color="#FC842D" bg="white" />
     </>
   );
 };
