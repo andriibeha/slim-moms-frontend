93c9b448e8c3f16ad1d8baf642a9460a2a2f14eb
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-12-17 23:01:00 +0200

    Fixed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
 // import Loader from 'components/Loader/Loader';
 import { Logo } from 'components/Logo';
-// import { Button } from 'components/Button';
 import { Button } from 'components/Button';
 import { AppNavigation } from 'components/Navigation';
 
@@ -10,7 +9,6 @@ export const Home = () => {
       <Logo />
       {/* <Loader /> */}
       <Button text="Hello" />
-      {/* <Button text="Helloo" /> */}
       <AppNavigation />
     </>
   );
