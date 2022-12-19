3823036b83773a80436ee81d3147d683897eba75
Kateryna Klimchuk <ketyclima@gmail.com>
2022-12-19 19:37:20 +0100

    feature/DiaryPage: Add all imports to Diary page, wrap the page into the Box

--- src/pages/Diary/index.jsx
+++ src/pages/Diary/index.jsx
@@ -1,3 +1,22 @@
+// import {DiaryDateСalendar},
+// import {DiaryAddProductForm}
+// import {DiaryProductsList}
+
+// import { authSelectors } from '../../redux/auth/auth-selectors';
+// import { useSelector } from 'react-redux';
+
+import { Box } from 'components/Box';
+import { UserInfo } from 'components/UserInfo';
+
 export const Diary = () => {
-  return <div>Diary</div>;
+  // const isUserLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  return (
+    <Box>
+      <UserInfo />
+      {/* // <DiaryDateСalendar/> */}
+      {/* // <DiaryAddProductForm/> */}
+      {/* // <DiaryProductsList /> */}
+    </Box>
+  );
 };
