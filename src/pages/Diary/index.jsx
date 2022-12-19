8bee4640fdc3e7cd72b135910741cb76f8883c99
Kateryna Klimchuk <ketyclima@gmail.com>
2022-12-19 20:44:06 +0100

    feature/DiaryPage: Add DiaryDateCalendar into Diary page

--- src/pages/Diary/index.jsx
+++ src/pages/Diary/index.jsx
@@ -1,8 +1,7 @@
 import { DiaryProductsList } from 'components/DiaryProductsList';
 
-// import {DiaryDateСalendar},
-// import {DiaryAddProductForm}
-// import {DiaryProductsList}
+import { DiaryDateСalendar } from 'components/DiaryDateСalendar';
+// import { DiaryAddProductForm } from 'components/DiaryAddProductForm';
 
 // import { authSelectors } from '../../redux/auth/auth-selectors';
 // import { useSelector } from 'react-redux';
@@ -16,8 +15,8 @@ export const Diary = () => {
   return (
     <Box>
       <UserInfo />
-      {/* // <DiaryDateСalendar/> */}
-      {/* // <DiaryAddProductForm/> */}
+      <DiaryDateСalendar />
+      {/* <DiaryAddProductForm /> */}
       <DiaryProductsList />
     </Box>
   );
