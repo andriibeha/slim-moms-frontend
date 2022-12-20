e8ee71402259a22ff7fc62b4e0140e404f60091f
MariiaLutska <myklush@ukr.net>
2022-12-20 18:18:25 +0200

    font

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -9,9 +9,7 @@ export const GlobalStyles = css`
   }
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-      'Helvetica Neue', sans-serif;
+    font-family: 'Verdana', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
