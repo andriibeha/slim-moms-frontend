cd9c10a4682a07e813feef62b32fdf9016d9e155
NataliaMalynovska <natalimalinovska57@gmail.com>
2022-12-22 09:20:11 +0200

    bar+-done

--- src/pages/Diary/index.jsx
+++ src/pages/Diary/index.jsx
@@ -1,6 +1,8 @@
 import { DiaryAddProductForm } from 'components/DiaryAddProductForm';
 import { DiaryDateCalendar } from 'components/DiaryDateСalendar';
 import { DiaryProductsList } from 'components/DiaryProductsList';
+import { RightSideBar } from 'components/RightSideBar';
+
 import {
   DiaryStyled,
   Wrapper,
@@ -8,6 +10,7 @@ import {
   DiaryCalendar,
   MobileWrapper,
   MobileButton,
+  DiaryStyledPage,
 } from './Diary.styled';
 import { useWindowResize } from 'hooks/useWindowResize';
 // import { authSelectors } from '../../redux/auth/auth-selectors';
@@ -17,23 +20,26 @@ export const Diary = () => {
   const { width } = useWindowResize();
 
   return (
-    <DiaryStyled>
-      <DiaryCalendar>
-        <DiaryDateCalendar />
-      </DiaryCalendar>
-      {width > 767 ? (
-        <Wrapper>
-          <DiaryAdd>
-            <DiaryAddProductForm />
-          </DiaryAdd>
-          <DiaryProductsList />
-        </Wrapper>
-      ) : (
-        <MobileWrapper>
-          <DiaryProductsList />
-          <MobileButton to="/add">+</MobileButton>
-        </MobileWrapper>
-      )}
-    </DiaryStyled>
+    <DiaryStyledPage>
+      <DiaryStyled>
+        <DiaryCalendar>
+          <DiaryDateCalendar />
+        </DiaryCalendar>
+        {width > 767 ? (
+          <Wrapper>
+            <DiaryAdd>
+              <DiaryAddProductForm />
+            </DiaryAdd>
+            <DiaryProductsList />
+          </Wrapper>
+        ) : (
+          <MobileWrapper>
+            <DiaryProductsList />
+            <MobileButton to="/add">+</MobileButton>
+          </MobileWrapper>
+        )}
+      </DiaryStyled>
+      <RightSideBar />
+    </DiaryStyledPage>
   );
 };
