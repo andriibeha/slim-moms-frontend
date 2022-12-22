6dfce1862d2e308933b49f4abf68d3e07d1b6a01
Andrey1914 <anlikajw@gmail.com>
2022-12-22 10:47:07 +0100

    wrote the redirect logic

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const Logo = () => {
   return (
+    // Якщо користувач isLoggedIn тоді Link направляє на сторінку "/Diary", а якщо !isLoggedIn - направляє на "/login"
     <Link to="/login">
       <Box>
         <Img src={logo} alt="main logo" />
