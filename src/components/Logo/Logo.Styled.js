d9da6a27e9175911851a4c1c24ba2000a3f1bb41
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-12-24 15:03:35 +0200

    Fixed logo picture
    Змінив логотип і його стилізацію

--- src/components/Logo/Logo.Styled.js
+++ src/components/Logo/Logo.Styled.js
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { theme } from 'components/Theme';
 
 export const Box = styled('div')`
   display: flex;
@@ -7,43 +6,5 @@ export const Box = styled('div')`
 
   @media screen and (min-width: 1280px) {
     align-items: end;
-  }
-`;
-export const LogoStyled = styled('p')`
-  padding: 0px;
-  margin: 0px;
-  margin-left: 0px;
-  color: ${theme.colors.textSecond};
-  display: none;
-
-  @media screen and (min-width: 768px) {
-    margin-left: 10px;
-    font-size: ${theme.fontSizes.m};
-    display: inherit;
-  }
-
-  @media screen and (min-width: 1280px) {
-    margin-left: -11px;
-    font-size: ${theme.fontSizes.l};
-  }
-`;
-export const Span = styled('span')`
-  color: ${theme.colors.accent};
-  @media screen and (min-width: 768px) {
-    font-size: ${theme.fontSizes.m};
-  }
-
-  @media screen and (min-width: 1280px) {
-    font-size: ${theme.fontSizes.l};
-  }
-`;
-
-export const Img = styled('img')`
-  width: 44px;
-  height: 44px;
-
-  @media screen and (min-width: 1280px) {
-    width: 70px;
-    height: 66px;
   }
 `;
